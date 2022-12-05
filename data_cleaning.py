62363de4456327b7654bf5c8e90f0a255c975a9a
nub69 <shabik.ashab@gmail.com>
2022-12-05 18:30:45 +0600

    predict if a employee will left the company or not

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
 data = pd.read_csv('HR_comma_sep.csv')
 
 # missing data for any row or any columns
@@ -38,5 +41,28 @@ final_data = merged.drop(['Department'], axis='columns')
 # show data on a scatter plot
 # plt.scatter(x=final_data.salary, y=final_data.left)
 # plt.scatter(x=final_data.satisfaction_level, y=final_data.left)
-plt.scatter(x=final_data.time_spend_company, y=final_data.left)
+# plt.scatter(x=final_data.time_spend_company, y=final_data.left)
+# plt.show()
+
+x = final_data.drop('left', axis='columns')
+y = final_data.left
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+accuracy = model.score(x_test, y_test)
+print(accuracy)
+
+# confusion matrics
+y_predicted = model.predict(x_test)
+confusion = confusion_matrix(y_test, y_predicted)
+# print(confusion)
+
+plot_confusion_matrix(model, x_test, y_test)
 plt.show()
+
+
+
+
