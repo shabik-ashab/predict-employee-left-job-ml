22e3c730e8c0f0cfe68e566f1ea3db548235c208
nub69 <shabik.ashab@gmail.com>
2022-12-05 17:33:21 +0600

    replace salary data with int

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,23 @@
-import pandas
+import pandas as pd
 
-data = pandas.read_csv('HR_comma_sep.csv')
+data = pd.read_csv('HR_comma_sep.csv')
 
 # missing data for any row or any columns
-print(data.isnull().values.any())
+# print(data.isnull().values.any())
+
+# check data type
+# print(data.dtypes)
+
+# print(data.salary.unique())
+# print(data.Department.unique())
+
+clean_up_values = {
+    'salary':{
+        'low': 1,
+        'medium': 2,
+        'high': 3
+    }
+}
+
+data.replace(clean_up_values, inplace=True)
+print(data)
